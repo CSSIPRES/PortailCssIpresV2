7a17611b9473dc10618d1e01dfecb0ccd87344d3
Olale Sam <olalesam87@gmail.com>
2020-05-18 14:15:00 +0000

    add configuration langage in file MailService

--- src/main/java/com/secusociale/portail/service/MailService.java
+++ src/main/java/com/secusociale/portail/service/MailService.java
@@ -77,7 +77,7 @@ public class MailService {
             log.debug("Email doesn't exist for user '{}'", user.getLogin());
             return;
         }
-        Locale locale = Locale.forLanguageTag(user.getLangKey());
+        Locale locale = Locale.forLanguageTag("FR");
         Context context = new Context(locale);
         context.setVariable(USER, user);
         context.setVariable(BASE_URL, jHipsterProperties.getMail().getBaseUrl());
