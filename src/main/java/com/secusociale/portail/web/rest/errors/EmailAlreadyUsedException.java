b156d5aaec5242436380a0bc2451b890a39434c4
ekebe <kebe1702@gmail.com>
2020-06-04 14:44:43 +0000

    Update Registration error English to French

--- src/main/java/com/secusociale/portail/web/rest/errors/EmailAlreadyUsedException.java
+++ src/main/java/com/secusociale/portail/web/rest/errors/EmailAlreadyUsedException.java
@@ -5,6 +5,6 @@ public class EmailAlreadyUsedException extends BadRequestAlertException {
     private static final long serialVersionUID = 1L;
 
     public EmailAlreadyUsedException() {
-        super(ErrorConstants.EMAIL_ALREADY_USED_TYPE, "Email déjà utilisé!", "userManagement", "emailexists");
+        super(ErrorConstants.EMAIL_ALREADY_USED_TYPE, "Cet email est déjà utilisé ! Veuillez en choisir un autre.", "userManagement", "emailexists");
     }
 }
--- src/main/java/com/secusociale/portail/web/rest/errors/LoginAlreadyUsedException.java
+++ src/main/java/com/secusociale/portail/web/rest/errors/LoginAlreadyUsedException.java
@@ -5,6 +5,6 @@ public class LoginAlreadyUsedException extends BadRequestAlertException {
     private static final long serialVersionUID = 1L;
 
     public LoginAlreadyUsedException() {
-        super(ErrorConstants.LOGIN_ALREADY_USED_TYPE, "Login déja utilisé !", "userManagement", "userexists");
+        super(ErrorConstants.LOGIN_ALREADY_USED_TYPE, "Cet login est déjà utilisé ! Veuillez en choisir un autre.", "userManagement", "userexists");
     }
 }
