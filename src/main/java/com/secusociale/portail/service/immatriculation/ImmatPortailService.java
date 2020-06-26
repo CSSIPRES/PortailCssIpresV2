92cc9b7413de7843f8b56cb79498a14fd5acacaa
ekebe <kebe1702@gmail.com>
2020-06-26 13:54:36 +0000

    update document upload

--- src/main/java/com/secusociale/portail/service/immatriculation/ImmatPortailService.java
+++ src/main/java/com/secusociale/portail/service/immatriculation/ImmatPortailService.java
@@ -79,7 +79,7 @@ public class ImmatPortailService {
 		input.setEmployerQuery(immatriculation.getInput().getEmployerQuery());
 		input.setMainRegistrationForm(immatriculation.getInput().getMainRegistrationForm());
 		input.setLegalRepresentativeForm(immatriculation.getInput().getLegalRepresentativeForm());
-		input.setDocuments(immatriculation.getInput().getDocuments());
+		//input.setDocuments(immatriculation.getInput().getDocuments());
 		
 		System.out.println("Documents portails:  "+immatriculation.getInput().getDocuments());
 
@@ -99,58 +99,58 @@ public class ImmatPortailService {
                     	 System.out.println("Registre de commerce:  "+immatriculation.getInput().getDocuments().getRegistreCommerce());
                     	immatriculation.getInput().getDocuments().getRegistreCommerce().setUrl(this.documentUrlService.uploadedDocument(BASE64DecodedMultipartFile.base64ToMultipart(immatriculation.getInput().getDocuments().getRegistreCommerce().getUrl(),"registre_commerce")));
                     }
-                    if(immatriculation.getInput().getDocuments().getDeclarationEtablissement() != null){
+                    if(immatriculation.getInput().getDocuments().getDeclarationEtablissement() != null && immatriculation.getInput().getDocuments().getDeclarationEtablissement().getUrl() != "" ){
                         immatriculation.getInput().getDocuments().getDeclarationEtablissement().setUrl(this.documentUrlService.uploadedDocument(BASE64DecodedMultipartFile.base64ToMultipart(immatriculation.getInput().getDocuments().getDeclarationEtablissement().getUrl(),"declaration_etablissement")));
                     }
-                    if(immatriculation.getInput().getDocuments().getPhotocopieStatus() != null){
+                    if(immatriculation.getInput().getDocuments().getPhotocopieStatus() != null && immatriculation.getInput().getDocuments().getPhotocopieStatus().getUrl() != ""){
                         immatriculation.getInput().getDocuments().getPhotocopieStatus().setUrl(this.documentUrlService.uploadedDocument(BASE64DecodedMultipartFile.base64ToMultipart(immatriculation.getInput().getDocuments().getPhotocopieStatus().getUrl(),"photocopie_status")));
                     }
-                    if(immatriculation.getInput().getDocuments().getDecretMinisteriel() != null){
+                    if(immatriculation.getInput().getDocuments().getDecretMinisteriel() != null && immatriculation.getInput().getDocuments().getDecretMinisteriel().getUrl() != ""){
                         immatriculation.getInput().getDocuments().getDecretMinisteriel().setUrl(this.documentUrlService.uploadedDocument(BASE64DecodedMultipartFile.base64ToMultipart(immatriculation.getInput().getDocuments().getDecretMinisteriel().getUrl(),"decret_ministeriel")));
                     }
-                    if(immatriculation.getInput().getDocuments().getAvisImmatriculation() != null){
+                    if(immatriculation.getInput().getDocuments().getAvisImmatriculation() != null && immatriculation.getInput().getDocuments().getAvisImmatriculation().getUrl() != ""){
                         immatriculation.getInput().getDocuments().getAvisImmatriculation().setUrl(this.documentUrlService.uploadedDocument(BASE64DecodedMultipartFile.base64ToMultipart(immatriculation.getInput().getDocuments().getAvisImmatriculation().getUrl(),"avis_immatriculation")));
                     }
-                    if(immatriculation.getInput().getDocuments().getDmt() != null){
+                    if(immatriculation.getInput().getDocuments().getDmt() != null && immatriculation.getInput().getDocuments().getDmt().getUrl() != ""){
                         immatriculation.getInput().getDocuments().getDmt().setUrl(this.documentUrlService.uploadedDocument(BASE64DecodedMultipartFile.base64ToMultipart(immatriculation.getInput().getDocuments().getDmt().getUrl(),"dmt")));
                     }
-                    if(immatriculation.getInput().getDocuments().getContratsTravail() != null){
+                    if(immatriculation.getInput().getDocuments().getContratsTravail() != null && immatriculation.getInput().getDocuments().getContratsTravail().getUrl() != ""){
                         immatriculation.getInput().getDocuments().getContratsTravail().setUrl(this.documentUrlService.uploadedDocument(BASE64DecodedMultipartFile.base64ToMultipart(immatriculation.getInput().getDocuments().getContratsTravail().getUrl(),"contrats_travail")));
                     }
                     if(immatriculation.getInput().getDocuments().getCni() != null){
                         immatriculation.getInput().getDocuments().getCni().setUrl(this.documentUrlService.uploadedDocument(BASE64DecodedMultipartFile.base64ToMultipart(immatriculation.getInput().getDocuments().getCni().getUrl(),"cni")));
                     }
-                    if(immatriculation.getInput().getDocuments().getCarteIdentiteConsulaire() != null){
+                    if(immatriculation.getInput().getDocuments().getCarteIdentiteConsulaire() != null && immatriculation.getInput().getDocuments().getCarteIdentiteConsulaire().getUrl() != ""){
                         immatriculation.getInput().getDocuments().getCarteIdentiteConsulaire().setUrl(this.documentUrlService.uploadedDocument(BASE64DecodedMultipartFile.base64ToMultipart(immatriculation.getInput().getDocuments().getCarteIdentiteConsulaire().getUrl(),"carte_identite_consulaire")));
                     }
-                    if(immatriculation.getInput().getDocuments().getEtatRecensement() != null){
+                    if(immatriculation.getInput().getDocuments().getEtatRecensement() != null && immatriculation.getInput().getDocuments().getEtatRecensement().getUrl() != ""){
                         immatriculation.getInput().getDocuments().getEtatRecensement().setUrl(this.documentUrlService.uploadedDocument(BASE64DecodedMultipartFile.base64ToMultipart(immatriculation.getInput().getDocuments().getEtatRecensement().getUrl(),"etat_recensement")));
                     }
-                    if(immatriculation.getInput().getDocuments().getAttestationChomage() != null){
+                    if(immatriculation.getInput().getDocuments().getAttestationChomage() != null && immatriculation.getInput().getDocuments().getAttestationChomage().getUrl() != ""){
                         immatriculation.getInput().getDocuments().getAttestationChomage().setUrl(this.documentUrlService.uploadedDocument(BASE64DecodedMultipartFile.base64ToMultipart(immatriculation.getInput().getDocuments().getAttestationChomage().getUrl(),"attestation_chomage")));
                     }
-                    if(immatriculation.getInput().getDocuments().getBulletinsSalaire() != null){
+                    if(immatriculation.getInput().getDocuments().getBulletinsSalaire() != null && immatriculation.getInput().getDocuments().getBulletinsSalaire().getUrl() != ""){
                         immatriculation.getInput().getDocuments().getBulletinsSalaire().setUrl(this.documentUrlService.uploadedDocument(BASE64DecodedMultipartFile.base64ToMultipart(immatriculation.getInput().getDocuments().getBulletinsSalaire().getUrl(),"bulletins_salaire")));
                     }
-                    if(immatriculation.getInput().getDocuments().getCessationActivity() != null){
+                    if(immatriculation.getInput().getDocuments().getCessationActivity() != null && immatriculation.getInput().getDocuments().getCessationActivity().getUrl() != ""){
                         immatriculation.getInput().getDocuments().getCessationActivity().setUrl(this.documentUrlService.uploadedDocument(BASE64DecodedMultipartFile.base64ToMultipart(immatriculation.getInput().getDocuments().getCessationActivity().getUrl(),"cessation_activity")));
                     }
-                    if(immatriculation.getInput().getDocuments().getCarteNationaleIdentite() != null){
+                    if(immatriculation.getInput().getDocuments().getCarteNationaleIdentite() != null && immatriculation.getInput().getDocuments().getCarteNationaleIdentite().getUrl() != ""){
                         immatriculation.getInput().getDocuments().getCarteNationaleIdentite().setUrl(this.documentUrlService.uploadedDocument(BASE64DecodedMultipartFile.base64ToMultipart(immatriculation.getInput().getDocuments().getCarteNationaleIdentite().getUrl(),"carte_nationale_identite")));
                     }
-                    if(immatriculation.getInput().getDocuments().getDerniersBulletins() != null){
+                    if(immatriculation.getInput().getDocuments().getDerniersBulletins() != null && immatriculation.getInput().getDocuments().getDerniersBulletins().getUrl() != ""){
                         immatriculation.getInput().getDocuments().getDerniersBulletins().setUrl(this.documentUrlService.uploadedDocument(BASE64DecodedMultipartFile.base64ToMultipart(immatriculation.getInput().getDocuments().getDerniersBulletins().getUrl(),"derniers_bulletins")));
                     }
-                    if(immatriculation.getInput().getDocuments().getManuscriteAdressee() != null){
+                    if(immatriculation.getInput().getDocuments().getManuscriteAdressee() != null && immatriculation.getInput().getDocuments().getManuscriteAdressee().getUrl() != ""){
                         immatriculation.getInput().getDocuments().getManuscriteAdressee().setUrl(this.documentUrlService.uploadedDocument(BASE64DecodedMultipartFile.base64ToMultipart(immatriculation.getInput().getDocuments().getManuscriteAdressee().getUrl(),"manuscrite_adressee")));
                     }
-                    if(immatriculation.getInput().getDocuments().getPassportDoc() != null){
+                    if(immatriculation.getInput().getDocuments().getPassportDoc() != null && immatriculation.getInput().getDocuments().getPassportDoc().getUrl() != ""){
                         immatriculation.getInput().getDocuments().getPassportDoc().setUrl(this.documentUrlService.uploadedDocument(BASE64DecodedMultipartFile.base64ToMultipart(immatriculation.getInput().getDocuments().getPassportDoc().getUrl(),"passport_doc")));
                     }
-                    if(immatriculation.getInput().getDocuments().getPieceIdDoc() != null){
+                    if(immatriculation.getInput().getDocuments().getPieceIdDoc() != null && immatriculation.getInput().getDocuments().getPieceIdDoc().getUrl() != ""){
                         immatriculation.getInput().getDocuments().getPieceIdDoc().setUrl(this.documentUrlService.uploadedDocument(BASE64DecodedMultipartFile.base64ToMultipart(immatriculation.getInput().getDocuments().getPieceIdDoc().getUrl(),"piece_id_Doc")));
                     }
-                    if(immatriculation.getInput().getDocuments().getPieceIdGerantDoc() != null){
+                    if(immatriculation.getInput().getDocuments().getPieceIdGerantDoc() != null && immatriculation.getInput().getDocuments().getPieceIdGerantDoc().getUrl() != ""){
                         immatriculation.getInput().getDocuments().getPieceIdGerantDoc().setUrl(this.documentUrlService.uploadedDocument(BASE64DecodedMultipartFile.base64ToMultipart(immatriculation.getInput().getDocuments().getPieceIdGerantDoc().getUrl(),"piece_id_gerantDoc")));
                     }
 
