cf39615d5ded5e0ad338c8475b40009fb512aba8
ekebe <kebe1702@gmail.com>
2020-06-29 15:13:11 +0000

    update document upload

--- src/main/java/com/secusociale/portail/security/AuthoritiesConstants.java
+++ src/main/java/com/secusociale/portail/security/AuthoritiesConstants.java
@@ -10,6 +10,12 @@ public final class AuthoritiesConstants {
     public static final String USER = "ROLE_USER";
 
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
+    
+    public static final String EMPLOYEUR = "ROLE_EMPLOYEUR";
+    
+    public static final String SALARIE = "ROLE_SALARIE";
+    
+    public static final String ALLOCATAIRE = "ROLE_ALLOCATAIRE";
 
     private AuthoritiesConstants() {
     }
